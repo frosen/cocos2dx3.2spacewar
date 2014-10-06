7134ef47f4379c76b8cd719d8759a902a0cf1d8a
frosen <frosen_lu@126.com>
2014-10-06 13:04:45 +0800

    把在家制作的guiReader先放到网上

---
+++ widget/llyGUIReader.cpp
@@ -0,0 +1,316 @@
+#include "llyGUIReader.h"
+#include "ui/CocosGUI.h"
+
+//================
+
+USING_NS_CC;
+using namespace ui;
+using namespace cocostudio;
+
+static lly::GUIReader* sharedReader = nullptr;
+
+lly::GUIReader* lly::GUIReader::getInstance()
+{
+	if (!sharedReader)
+	{
+		sharedReader = new lly::GUIReader();
+	}
+	return sharedReader;
+}
+
+void lly::GUIReader::destroyInstance()
+{
+	CC_SAFE_DELETE(sharedReader);
+}
+
+cocos2d::ui::Widget* lly::GUIReader::widgetFromJsonFile( const char* fileName )
+{
+	std::string jsonpath;
+	rapidjson::Document jsonDict;
+	jsonpath = fileName;
+	//    jsonpath = CCFileUtils::getInstance()->fullPathForFilename(fileName);
+	size_t pos = jsonpath.find_last_of('/');
+	m_strFilePath = jsonpath.substr(0,pos+1);
+	std::string contentStr = FileUtils::getInstance()->getStringFromFile(jsonpath);
+	jsonDict.Parse<0>(contentStr.c_str());
+	if (jsonDict.HasParseError())
+	{
+		CCLOG("GetParseError %s\n",jsonDict.GetParseError());
+	}
+	Widget* widget = nullptr;
+	const char* fileVersion = DICTOOL->getStringValue_json(jsonDict, "version");
+	WidgetPropertiesReader * pReader = nullptr;
+	if (fileVersion)
+	{
+		int versionInteger = getVersionInteger(fileVersion);
+		if (versionInteger < 250)
+		{
+			pReader = new WidgetPropertiesReader0250();
+			widget = pReader->createWidget(jsonDict, m_strFilePath.c_str(), fileName);
+		}
+		else
+		{
+			pReader = new lly::WidgetPropertiesReader0300(); //此处修改
+			widget = pReader->createWidget(jsonDict, m_strFilePath.c_str(), fileName);
+		}
+	}
+	else
+	{
+		pReader = new WidgetPropertiesReader0250();
+		widget = pReader->createWidget(jsonDict, m_strFilePath.c_str(), fileName);
+	}
+
+	CC_SAFE_DELETE(pReader);
+	return widget;
+}
+
+void lly::GUIReader::registerTypeWithExName( const std::string& classType, cocos2d::ObjectFactory::Instance TypeIns, cocos2d::ObjectFactory::Instance ReaderIns, char ch )
+{
+	ObjectFactory* factoryCreate = ObjectFactory::getInstance();
+
+	std::string strCunstomType = "";
+	strCunstomType += ch;
+	strCunstomType += classType;	
+
+	ObjectFactory::TInfo t(strCunstomType, TypeIns);
+	factoryCreate->registerType(t);
+
+	strCunstomType.append("Reader");
+
+	ObjectFactory::TInfo t2(strCunstomType, ReaderIns);
+	factoryCreate->registerType(t2);
+}
+
+void lly::GUIReader::recordPointInMap( const char* name, Vec2 &point )
+{
+	m_mapPoint.insert(std::pair<char*, Vec2>(name, point));
+}
+
+cocos2d::Vec2 lly::GUIReader::getPoint( const char* name )
+{
+	return m_mapPoint.at(name);
+}
+
+//================================================
+lly::GUIReader::GUIReader() : cocostudio::GUIReader()
+{
+
+}
+
+lly::GUIReader::~GUIReader()
+{
+
+}
+
+//========================================
+
+cocos2d::ui::Widget* lly::WidgetPropertiesReader0300::widgetFromJsonDictionary( const rapidjson::Value& data )
+{
+	const char* classname = DICTOOL->getStringValue_json(data, "classname");
+	const rapidjson::Value& uiOptions = DICTOOL->getSubDictionary_json(data, "options");
+
+	//自己添加的内容开始====================================
+	const char* name = cocostudio::DICTOOL->getStringValue_json(uiOptions, "name");
+	CCLOG("%s is a %s, create", name, classname);
+
+	//判断name开头是否是PO，代表point，则在map里面记录此控件的名称和位置
+	if (name && name[0] == 'P' && name[1] == 'O')
+	{
+		//在json中获取x，y坐标点
+		float x = DICTOOL->getFloatValue_json(uiOptions, "x");
+		float y = DICTOOL->getFloatValue_json(uiOptions, "y");
+
+		//放入map中
+
+		//返回空值，因为这个控件就不显示于界面上了
+		return nullptr;
+	}
+
+	//判断name开头是不是EX，是的话就是自定义控件，否则就是原有控件
+	if (name && name[0] == 'E' && name[1] == 'X') 
+	{
+		std::string strTemp = "";
+		strTemp += name[2];
+		strTemp += classname;
+		classname = strTemp.c_str();
+	}
+	//结束======================
+
+	Widget* widget = this->createGUI(classname); //修改
+	//    CCLOG("classname = %s", classname);
+	std::string readerName = this->getWidgetReaderClassName(classname);
+
+	WidgetReaderProtocol* reader = this->createWidgetReaderProtocol(readerName);
+
+	if (reader)
+	{
+		// widget parse with widget reader
+		setPropsForAllWidgetFromJsonDictionary(reader, widget, uiOptions);
+	}
+	else
+	{
+		readerName = this->getWidgetReaderClassName(widget);
+
+		reader = dynamic_cast<WidgetReaderProtocol*>(ObjectFactory::getInstance()->createObject(readerName));
+
+		if (reader && widget) {
+			setPropsForAllWidgetFromJsonDictionary(reader, widget, uiOptions);
+
+			// 2nd., custom widget parse with custom reader
+			const char* customProperty = DICTOOL->getStringValue_json(uiOptions, "customProperty");
+			rapidjson::Document customJsonDict;
+			customJsonDict.Parse<0>(customProperty);
+			if (customJsonDict.HasParseError())
+			{
+				CCLOG("GetParseError %s\n", customJsonDict.GetParseError());
+			}
+			setPropsForAllCustomWidgetFromJsonDictionary(classname, widget, customJsonDict);
+		}else{
+			CCLOG("Widget or WidgetReader doesn't exists!!!  Please check your json file.");
+		}
+
+	}
+
+	int childrenCount = DICTOOL->getArrayCount_json(data, "children");
+	for (int i = 0; i < childrenCount; i++)
+	{
+		const rapidjson::Value& subData = DICTOOL->getDictionaryFromArray_json(data, "children", i);
+		cocos2d::ui::Widget* child = widgetFromJsonDictionary(subData);
+		if (child)
+		{
+			PageView* pageView = dynamic_cast<PageView*>(widget);
+			if (pageView)
+			{
+				pageView->addPage(static_cast<Layout*>(child));
+			}
+			else
+			{
+				ListView* listView = dynamic_cast<ListView*>(widget);
+				if (listView)
+				{
+					listView->pushBackCustomItem(child);
+				}
+				else
+				{
+					if (!dynamic_cast<Layout*>(widget))
+					{
+						if (child->getPositionType() == ui::Widget::PositionType::PERCENT)
+						{
+							child->setPositionPercent(Vec2(child->getPositionPercent().x + widget->getAnchorPoint().x, child->getPositionPercent().y + widget->getAnchorPoint().y));
+						}
+						child->setPosition(Vec2(child->getPositionX() + widget->getAnchorPointInPoints().x, child->getPositionY() + widget->getAnchorPointInPoints().y));
+					}
+					widget->addChild(child);
+				}
+			}
+		}
+	}
+	return widget;
+}
+
+//===========================================
+void lly::setWidgetFast( cocos2d::ui::Widget* widget, cocos2d::ui::Widget* father, char* childname, ... )
+{
+	//获得控件
+	va_list args;
+	va_start(args, childname);
+	widget = getChildWidget(father, childname, args);
+	va_end(args);
+}
+
+void lly::setWidgetFast( cocos2d::ui::Widget* widget, cocos2d::ui::Widget* father, cocos2d::ui::ccWidgetTouchCallback callback, char* childname, ... )
+{
+	//获得控件
+	va_list args;
+	va_start(args, childname);
+	widget = getChildWidget(father, childname, args);
+	va_end(args);
+
+	//给控件赋予触控回调
+	widget->addTouchEventListener(callback);
+}
+
+void lly::setWidgetFast( cocos2d::ui::Widget* widget, cocos2d::ui::Widget* father, cocos2d::ui::ccCheckBoxCallback callback, char* childname, ... )
+{
+	//获得控件
+	va_list args;
+	va_start(args, childname);
+	widget = getChildWidget(father, childname, args);
+	va_end(args);
+
+	//给控件赋予触控回调（复选框的）
+	(cocos2d::ui::CheckBox*)widget->addEventListener(callback);
+}
+
+void lly::setWidgetFast( cocos2d::ui::Widget* father, cocos2d::ui::ccWidgetTouchCallback callback, char* childname, ... )
+{
+	//声明一个widget类型
+	cocos2d::ui::Widget* wiTemp = nullptr;
+
+	//获得控件
+	va_list args;
+	va_start(args, childname);
+	wiTemp = getChildWidget(father, childname, args);
+	va_end(args);
+
+	//给控件赋予触控回调
+	wiTemp->addTouchEventListener(callback);
+}
+
+void lly::setWidgetFast( cocos2d::ui::Widget* father, cocos2d::ui::ccCheckBoxCallback callback, char* childname, ... )
+{
+	//声明一个和widget类型一致的指针
+	cocos2d::ui::Widget* wiTemp = nullptr;
+
+	//获得控件
+	va_list args;
+	va_start(args, childname);
+	wiTemp = getChildWidget(father, childname, args);
+	va_end(args);
+
+	//给控件赋予触控回调（复选框的）
+	(cocos2d::ui::CheckBox*)wiTemp->addEventListener(callback);
+}
+
+cocos2d::ui::Widget* lly::getChildWidget( cocos2d::ui::Widget* father, char* pch, ... )
+{
+	do 
+	{
+		//确保父层为空，否则返回空值
+		if (!father) break;
+
+		//父层的第一层子控件
+		cocos2d::ui::Widget* wiChild = static_cast<cocos2d::ui::Widget*>(father->getChildByName(pch));
+		if (!wiChild) break;
+
+		//读入可变参数，每个参数往下读取一层的子控件
+		va_list params;
+		va_start(params, pch);
+		char* childname;
+
+		while (true)
+		{
+			childname = va_arg(params, char*);
+
+			if (childname == nullptr) break;
+
+			wiChild = static_cast<cocos2d::ui::Widget*>(wiChild->getChildByName(childname));
+			if (wiChild == nullptr) break;
+		}
+
+		va_end(params);
+
+		if (!wiChild) break;
+
+		//返回子控件
+		return wiChild;
+
+	} while (0);
+	
+	//如果子控件为空则打断
+	CCAssert(false, "@wrong widget child");
+	return nullptr;
+}
+
+
+
---
+++ widget/llyGUIReader.h
@@ -0,0 +1,80 @@
+/*
+名称：llyGUIReader.h
+内容：自定义的cocostudio文件读取器，可以在文件名称前加上EX，就可以使用对应的新控件
+		自定义控件需要提前注册到读取器内
+		如果控件名称前增加PO，则此控件不加入父层中，而是获得其xy坐标放入列表，
+
+制作：卢乐颜
+日期：2014.5.1
+*/
+
+#ifndef _LLY_GUI_READER_H_
+#define _LLY_GUI_READER_H_
+
+#include "cocos2d.h"
+#include "cocostudio/CocoStudio.h"
+
+namespace lly{
+
+class GUIReader : public cocostudio::GUIReader
+{
+public:
+	static GUIReader* getInstance(); //覆盖
+	static void destroyInstance(); //覆盖
+	cocos2d::ui::Widget* widgetFromJsonFile(const char* fileName); //覆盖
+
+	//自定义的控件注册到reader里
+	//指定类型名称，类型创建函数指针，类型解析函数指针，还有特殊标识字符
+	//所有name开头为EX的为自定义类，EX后紧跟着的字符为特殊标识（默认为 _ ）
+	void registerTypeWithExName(const std::string& classType,
+		cocos2d::ObjectFactory::Instance TypeIns,
+		cocos2d::ObjectFactory::Instance ReaderIns,
+		char ch = '_'); 
+
+	//根据json文件里面所有PO打头的控件，记录他们的position，以供程序里使用
+	void recordPointInMap(const char* name, cocos2d::Vec2 &point);
+
+	//根据名称，获取坐标点
+	cocos2d::Vec2 getPoint(const char* name);
+
+protected:
+	GUIReader();
+	virtual ~GUIReader();
+
+protected:
+	std::map<char*, cocos2d::Vec2> m_mapPoint; //放置坐标点的map
+};
+
+class WidgetPropertiesReader0300 : public cocostudio::WidgetPropertiesReader0300
+{
+public:
+	WidgetPropertiesReader0300(){};
+	virtual ~WidgetPropertiesReader0300(){};
+
+	virtual cocos2d::ui::Widget* widgetFromJsonDictionary(const rapidjson::Value& dic); //覆盖
+};
+
+//=============================
+//快速获得cocostudio的控件
+void setWidgetFast(cocos2d::ui::Widget* widget, cocos2d::ui::Widget* father, char* childname, ...);
+
+//重载版本，给控件增加触摸回调
+void setWidgetFast(cocos2d::ui::Widget* widget, cocos2d::ui::Widget* father, cocos2d::ui::ccWidgetTouchCallback callback, char* childname, ...);
+
+//重载，回调是复选框的回调
+void setWidgetFast(cocos2d::ui::Widget* widget, cocos2d::ui::Widget* father, cocos2d::ui::ccCheckBoxCallback callback, char* childname, ...);
+
+//重载版本，给控件增加触摸回调，不需要给控件声明一个指针控制它
+void setWidgetFast(cocos2d::ui::Widget* father, cocos2d::ui::ccWidgetTouchCallback callback, char* childname, ...);
+
+//重载版本，给控件增加触摸回调，回调是复选框的回调，不需要给控件声明一个指针控制它
+void setWidgetFast(cocos2d::ui::Widget* father, cocos2d::ui::ccCheckBoxCallback callback, char* childname, ...);
+
+//获取子控件，根据父层和父层下每层子控件的名称
+//如果出错则会打断
+cocos2d::ui::Widget* getChildWidget(cocos2d::ui::Widget* father, char* pch, ...);
+
+} // namespace lly
+
+#endif // _LLY_GUI_READER_H_
+
